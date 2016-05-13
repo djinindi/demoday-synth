cd31e95f215c9c9a0e6c6b6c09e10052c53929c0
Jonas Jacobsen <joj@bettercollective.com>
2016-05-13 15:38:48 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const electron = require('electron');
+const {app, BrowserWindow} = electron;
+let mainWindow;
+
+function createWindow() {
+	mainWindow = new BrowserWindow({width: 800, height: 535});
+	mainWindow.loadURL('file://' + __dirname + '/index.html');
+	//mainWindow.webContents.openDevTools();
+	mainWindow.on('closed', () => {
+		mainWindow = null;
+	});
+}
+
+app.on('ready', createWindow);
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+app.on('activate', () => {
+	if (mainWindow === null) {
+		createWindow();
+	}
+});
